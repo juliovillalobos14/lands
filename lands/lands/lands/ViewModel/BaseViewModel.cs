7f12e676e256be026aac26805edf594d17132911
juliovillalobos14 <julio.gamino@tectijuana.edu.mx>
2019-04-14 20:00:19 -0700

    4to cambio

---
+++ lands/lands/lands/ViewModel/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+namespace lands.ViewModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    //Tiene la lógica para no tener la necesidad de 
+    //copiar y pegar los elementos que se encuentran 
+    //en la LoginViewModel
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetValue<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+            {
+                return;
+            }
+            backingField = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
--- lands/lands/lands/ViewModel/LoginViewModel.cs
+++ lands/lands/lands/ViewModel/LoginViewModel.cs
@@ -1,14 +1,26 @@
 ﻿
 namespace lands.ViewModel
 {
+    using GalaSoft.MvvmLight.Command;
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Text;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    //Heredamos una interfaz para actualizas
+    public class LoginViewModel : BaseViewModel
     {
-        #region
+        
+
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+        #region Properties
         public string Email
         {
             get;
@@ -16,26 +28,88 @@ namespace lands.ViewModel
         }
         public string Password
         {
-            get;
-            set;
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
         }
         public bool IsRunning
         {
-            get;
-            set;
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
         }
         public bool IsRemembered
         {
             get;
             set;
         }
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
         #endregion
 
         #region Commands
         public ICommand LoginCommand
         {
-            get;
-            set;
+            get
+            {
+                //Este objeto usa el nugget ligh lymbs
+                return new RelayCommand(Login);
+            }
+        }
+        #endregion
+
+        #region Metodos
+        //Método de login
+        //Método asincrono
+        private async void Login()
+        {
+            //Validar que el usuario haya dígitado el email y password
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an password",
+                    "Accept");
+            }
+
+            this.isRunning = true;
+            this.IsEnabled = false;
+
+
+            //Mover a otra página cuando haga login
+            if (this.Email != "jzuluaga55@gmail.com" || this.Password != "1234")
+            {
+
+                this.isRunning = false;
+                this.IsEnabled = true;
+
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+
+                return;
+            }
+
+            this.isRunning = false;
+            this.IsEnabled = true;
+
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "OK",
+                    "Enter",
+                    "Accept");
         }
         #endregion
 
@@ -43,6 +117,7 @@ namespace lands.ViewModel
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.isEnabled = true;
         }
         #endregion
 
